afd22eb698d3b41d6c45f8be02b04a5b5fde730e
duncansu <duncan2001900220@gmail.com>
2022-05-10 14:29:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/okarepro/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.okarepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void helpmethod(View view) {
+    }
+
+    public void healthymethod(View view) {
+    }
+
+    public void medicinemethod(View view) {
+    }
+
+    public void cloudmethod(View view) {
+    }
+}
