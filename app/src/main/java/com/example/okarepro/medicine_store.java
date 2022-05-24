710820ea2739bbec8b524e253417a8d15811ca64
duncansu <duncan2001900220@gmail.com>
2022-05-24 22:21:41 +0800

    5/24幹你娘一直當機是沙小

--- app/src/main/java/com/example/okarepro/medicine_store.java
+++ app/src/main/java/com/example/okarepro/medicine_store.java
@@ -5,8 +5,10 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,9 +17,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.opencsv.CSVReader;
 
@@ -57,44 +61,129 @@ public class medicine_store extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-
                 //偵測手機經緯度
 
-                if(Build.VERSION.SDK_INT>Build.VERSION_CODES.M){
-                    if (ActivityCompat.checkSelfPermission(medicine_store.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(medicine_store.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!isGpsAble(locationManager)) {
+                    Toast.makeText(medicine_store.this, "請打開GPS", Toast.LENGTH_SHORT).show();
                 }
-                locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 0, medicine_store.this);
-                String localProvider = "";
-                Location location = locationManager.getLastKnownLocation(localProvider);
-                if (location != null){
-                    String address = "緯度："+location.getLatitude()+"經度："+location.getLongitude();
-                    System.out.print(address);
+                if (ContextCompat.checkSelfPermission(medicine_store.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {//未開啟定位權限
+                    //開啟定位權限,200是標識碼
+                    ActivityCompat.requestPermissions(medicine_store.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+                } else {
+                    startLocation();
+                    Toast.makeText(medicine_store.this, "已開啟定位權限", Toast.LENGTH_LONG).show();
                 }
-                else {
-                    System.out.print("bye bye");
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, mListener);
-                   locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, mListener);
+
+                double myla=Double.parseDouble(startLocation().split(",")[1]);
+                double mylo=Double.parseDouble(startLocation().split(",")[3]);
+                double stla=121.570639840024;
+                double stlo=25.001390217006;
+
+                //System.out.print(String.valueOf(gps2m(myla,mylo,stla,stlo))+"m\n");
+            }
+
+            //定義一個更新顯示的方法
+            private String updateShow(Location location) {
+                if (location != null) {
+                    String sb = new String();
+                    sb+=("經度：" +","+ location.getLongitude() + ","+"緯度：" +","+ location.getLatitude());
+                    //System.out.println(sb);
+                    return sb;
+                } else {System.out.print("bye bye");
+                return "bye bye";
                 }
 
             }
 
+            @SuppressLint("MissingPermission")
+            private String startLocation() {
+                //從GPS獲取最近的定位資訊
+                Location lc = getLastKnownLocation();
+                String aa=updateShow(lc);
+                //設定間隔兩秒獲得一次GPS定位資訊
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 8, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        // 當GPS定位資訊發生改變時，更新定位
+                        updateShow(location);
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                        // 當GPS LocationProvider可用時，更新定位
+                        updateShow(locationManager.getLastKnownLocation(provider));
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        updateShow(null);
+                    }
+                });
+                return aa;
+            }
+
+            private Location getLastKnownLocation() {
+
+                locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+                List<String> providers = locationManager.getProviders(true);
+                Location bestLocation = null;
+                for (String provider : providers) {
+                    if (ActivityCompat.checkSelfPermission(medicine_store.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(medicine_store.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    }
+                    Location l = locationManager.getLastKnownLocation(provider);
+                    if (l == null) {
+                        continue;
+                    }
+                    if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                        // Found best last known location: %s", l);
+                        bestLocation = l;
+                    }
+                }
+                return bestLocation;
+            }
+
+            private boolean isGpsAble(LocationManager lm) {
+                return lm.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER) ? true : false;
+            }
+
+            private final double EARTH_RADIUS = 6378137.0;
+            private double gps2m(double lat_a, double lng_a, double lat_b, double lng_b) {
+                double radLat1 = (lat_a * Math.PI / 180.0);
+                double radLat2 = (lat_b * Math.PI / 180.0);
+                double a = radLat1-radLat2;
+                double b = (lng_a-lng_b) * Math.PI / 180.0;
+                double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)+
+                        Math.cos(radLat1) * Math.cos(radLat2)
+                                * Math.pow(Math.sin(b / 2), 2)));
+                s = s * EARTH_RADIUS;
+                s = Math.round(s * 10000) / 10000;
+                return s;
+            }
+
+
+
+
+
+
 
             public String readTextFile(InputStreamReader inputStream) throws IOException {
                     BufferedReader br=new BufferedReader(inputStream);
+                   // BufferedReader br1=new BufferedReader(inputStream);
                     String aa="";
                     while ((aa=br.readLine())!=null){
                         String item[] = aa.split(",");
                         /** 讀取 **/
-                        String  data1= item[0].trim();
-                        String  data2= item[1].trim();
-                        String  data3= item[2].trim();
-                        //System.out.print(data1+"\t"+ data2+"\t"+ data3+"\n");
+                        String  data1= item[1];
+                        String  data2= item[4];
+                        String  data3= item[5];
+                        if(gps2m(Double.parseDouble(startLocation().split(",")[1]),Double.parseDouble(startLocation().split(",")[3]),Double.parseDouble(data2),Double.parseDouble(data3))<1800){
+                        System.out.print(data1+"\t"+ data2+"\t"+ data3+"\n");}
                     }
                     br.close();
+
+
                     return aa;
                 }
 
@@ -111,14 +200,6 @@ public class medicine_store extends AppCompatActivity {
         });
     }
 
-
-    LocationListener mListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(@NonNull Location location) {
-            String address = "緯度：" + location.getLatitude() + "經度：" + location.getLongitude();
-            System.out.print(address);
-        }
-    };
 
 
 }
